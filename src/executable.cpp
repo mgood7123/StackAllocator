fda0914aea957d090a2fa639526708c9e431598f
smallville7123 <smallville7123@gmail.com>
2023-04-30 19:17:48 +1000

    test string

--- src/executable.cpp
+++ src/executable.cpp
@@ -5,5 +5,8 @@ int main () {
     SA::Allocator a;
     SA::DefaultAllocator * ap = a.alloc<SA::DefaultAllocator>();
     ap->alloc<int>()[0] = 567884;
+    auto * str = ap->alloc<std::string>();
+    str[0] = "hello";
+    SA::Logi(str[0]);
     return 0;
 }
